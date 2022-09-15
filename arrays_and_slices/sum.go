bf10986821c754ff927819a54dff4d0653095ce2
Jakub Gałecki <goathy.dev@gmail.com>
2022-09-15 21:17:20 +0000

    feat(arrays&slices): add functions that sums
    * Add functions that sums slice values.

---
+++ arrays_and_slices/sum.go
@@ -0,0 +1,29 @@
+package sum
+
+func Sum(numbers []int) (sum int) {
+	for _, number := range numbers {
+		sum += number
+	}
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			// numbers[1:] means that take element at index 1 and rest of the slice values
+			sums = append(sums, Sum(numbers[1:]))
+		}
+	}
+	return
+}

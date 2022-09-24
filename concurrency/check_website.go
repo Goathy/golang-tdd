4385a606b8b687ca02ef8e4bc70d35f6ea7e997b
Jakub Gałecki <goathy.dev@gmail.com>
2022-09-24 15:32:55 +0000

    feat(concurrency): working with concurrency & channels

---
+++ concurrency/check_website.go
@@ -0,0 +1,13 @@
+package concurrency
+
+import "net/http"
+
+func CheckWebsite(url string) bool {
+	response, err := http.Head(url)
+
+	if err != nil {
+		return false
+	}
+
+	return response.StatusCode == http.StatusOK
+}
